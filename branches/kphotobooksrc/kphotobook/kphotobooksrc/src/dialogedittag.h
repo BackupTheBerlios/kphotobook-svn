2f9f89da6c2c92b3e21108bd65a246f3d236a2f6
(no author) <(no author)@99f15ee9-15f3-0310-9699-9f69ca06e6ba>
2004-03-07 18:51:55 +0000

    This commit was manufactured by cvs2svn to create branch 'kphotobooksrc'.

---
+++ branches/kphotobooksrc/kphotobook/kphotobooksrc/src/dialogedittag.h
@@ -0,0 +1,71 @@
+/***************************************************************************
+ *   Copyright (C) 2003 by Michael Christen                                *
+ *   starcube@my-mail.ch                                                   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#ifndef DIALOGEDITTAG_H
+#define DIALOGEDITTAG_H
+
+#include <kdialogbase.h>
+#include <klineedit.h>
+#include <kcombobox.h>
+
+#include <qstring.h>
+#include <qpushbutton.h>
+
+class TagTreeNode;
+
+/**
+ * The dialog to create a now tag.
+ *
+ * CVS-ID $Id: dialogedittag.h,v 1.1 2004/03/07 18:52:06 starcube Exp $
+ */
+class DialogEditTag : public KDialogBase {
+
+Q_OBJECT
+
+public:
+    DialogEditTag(QWidget *parent, TagTreeNode* parentNode, const char *name);
+
+    ~DialogEditTag();
+
+    QString tagName() {
+        return m_nameLineEdit->text();
+    }
+
+    QString tagIcon() {
+        return m_iconLineEdit->text();
+    }
+
+private slots:
+    void slotNameChanged(const QString& text);
+    void slotIconTextChanged(const QString& text);
+    void slotIconButtonClicked();
+
+private:
+    TagTreeNode* m_tagTreeNode;
+
+    KComboBox* m_typeComboBox;
+    KLineEdit* m_nameLineEdit;
+    KLineEdit* m_iconLineEdit;
+    QPushButton* m_iconButton;
+
+    void validate();
+};
+
+#endif
---
+++ branches/kphotobooksrc/kphotobook/kphotobooksrc/src/kphotobookview.cpp
@@ -0,0 +1,249 @@
+/*
+ * Copyright (C) 2003 Michael Christen <starcube@my-mail.ch>
+ */
+
+#include "kphotobookview.h"
+
+#include "configuration.h"
+#include "tagtree.h"
+#include "sourcedirtree.h"
+#include "kphotobook.h"
+#include "constants.h"
+#include "sourcedir.h"
+#include "file.h"
+#include "tagtreenodesourcedir.h"
+
+#include <qobject.h>
+#include <qpainter.h>
+#include <qlayout.h>
+#include <qstring.h>
+#include <qpopupmenu.h>
+
+#include <kurl.h>
+
+#include <ktrader.h>
+#include <klibloader.h>
+#include <kmessagebox.h>
+#include <krun.h>
+#include <klocale.h>
+#include <kdebug.h>
+#include <kprocess.h>
+#include <kfileiconview.h>
+
+
+KPhotoBookView::KPhotoBookView(QWidget *parent)
+    : QWidget(parent) {
+
+    // store casted pointer to the photobook
+    m_photobook = dynamic_cast<KPhotoBook*>(parent);
+
+    // setup our layout manager to automatically add our widgets
+    QHBoxLayout* topLayout = new QHBoxLayout(this, 5, 5, "topLayout");
+    topLayout->setAutoAdd(true);
+
+    // splitter
+    m_split = new QSplitter(Qt::Horizontal, this, "split");
+    m_split->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+
+    // prepare the tagtree container
+    QWidget* outerPanel = new QWidget(m_split, "outerPanel");
+    QVBoxLayout* outerPanelLayout = new QVBoxLayout(outerPanel, 5, 5, "outerPanelLayout");
+    outerPanelLayout->setAutoAdd(true);
+
+    // treesplitter
+    m_treesplit = new QSplitter(Qt::Vertical, outerPanel, "treesplit");
+    m_treesplit->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+
+    // append the tagtree
+    m_tagtree = new TagTree(m_treesplit, m_photobook, "tagtree");
+
+    // append the sourcedirTree
+    m_sourcedirTree = new SourceDirTree(m_treesplit, m_photobook, "sourcedirTree");
+
+    // main panel
+    QWidget* mainPanel = new QWidget(m_split, "mainPanel");
+    QVBoxLayout* mainLayout = new QVBoxLayout(mainPanel, 5, 5, "mainLayout");
+    mainLayout->setAutoAdd(true);
+
+    // image size panel
+    QWidget* imageSizePanel = new QWidget(mainPanel, "imageSizePanel");
+    QHBoxLayout* imageSizePanelLayout = new QHBoxLayout(imageSizePanel, 5, 5, "imageSizePanelLayout");
+    imageSizePanelLayout->setAutoAdd(true);
+
+    new QLabel(i18n("Image size:"), imageSizePanel, "imageSizeLabel");
+
+    m_imageSizeSlider = new QSlider(4, 20, 1, Configuration::getInstance()->previewSize()/8, Qt::Horizontal, imageSizePanel, "imageSizeSlider");
+    m_imageSizeSlider->setTickmarks(QSlider::Above);
+    QObject::connect(m_imageSizeSlider, SIGNAL(valueChanged(int)), this, SLOT(slotImageSizeSliderChanged(int)));
+    QObject::connect(m_imageSizeSlider, SIGNAL(sliderPressed()), this, SLOT(slotImageSizeSliderPressed()));
+    QObject::connect(m_imageSizeSlider, SIGNAL(sliderReleased()), this, SLOT(slotImageSizeSliderReleased()));
+
+    m_currentImageSize = new QLabel("", imageSizePanel, "currentImageSize");
+    updateCurrentImageSizeLabel();
+
+    // file preview
+    m_fileView = new KFileIconView(mainPanel, "fileIconView");
+    m_fileView->setSelectionMode(KFile::Multi);
+    m_fileView->setResizeMode(KFileIconView::Adjust);
+    m_fileView->showPreviews();
+    m_fileView->setWordWrapIconText(false);
+    updateCurrentImageSize();
+
+    QObject::connect(m_fileView, SIGNAL(doubleClicked(QIconViewItem*, const QPoint&)), this, SLOT(slotShowCurrentImage()));
+    QObject::connect(m_fileView, SIGNAL(selectionChanged ()), m_photobook, SLOT(slotFileSelectionChanged()));
+
+    // set splitter position
+    m_split->setSizes(Configuration::getInstance()->mainSplitterSizes());
+    m_treesplit->setSizes(Configuration::getInstance()->treeSplitterSizes());
+}
+
+
+KPhotoBookView::~KPhotoBookView() {
+
+    kdDebug() << "KPhotoBookView::~KPhotoBookView() invoked..." << endl;
+
+    // remove the current previewed files
+    //m_fileView->clearView();
+
+    // all components are deleted automagically by the destructor of QWidget
+}
+
+
+TagTreeNodeSourceDir* KPhotoBookView::selectedSourceDir() {
+
+    QListViewItem* currentItem = m_sourcedirTree->currentItem();
+    if (typeid(*currentItem) != typeid(TagTreeNodeSourceDir)) {
+        kdDebug() << "[KPhotoBookView::selectedSourceDir] sourcedirTree contains a node of other type than 'TagTreeNodeSourceDir'" << endl;
+        return 0;
+    }
+
+    return dynamic_cast<TagTreeNodeSourceDir*>(currentItem);
+}
+
+
+void KPhotoBookView::removeTagNode(TagTreeNode* node) {
+
+    // the node's destructor removes itself from the tree
+    delete node;
+}
+
+
+void KPhotoBookView::updateFiles() {
+
+    kdDebug() << "[KPhotoBookView::updateFiles] updating the displayed images." << endl;
+
+    // remove all displayed images
+    removeAllFiles();
+
+    // add the images
+    QPtrList<File>* fileList = m_photobook->files(0);
+
+    File* file;
+    for ( file = fileList->first(); file; file = fileList->next() ) {
+        // kdDebug() << "Adding file: " << file->fileInfo()->absFilePath() << endl;
+
+        m_fileView->insertItem(file);
+//        m_fileView->arrangeItemsInGrid();
+    }
+    kdDebug() << "[KPhotoBookView::updateFiles] ended." << endl;
+}
+
+
+void KPhotoBookView::storeConfiguration() {
+    // store slider position
+    Configuration::getInstance()->setPreviewSize(8*m_imageSizeSlider->value());
+    // store splitter sizes
+    Configuration::getInstance()->setMainSplitterSizes(m_split->sizes());
+    Configuration::getInstance()->setTreeSplitterSizes(m_treesplit->sizes());
+}
+
+
+//
+// private slots
+//
+void KPhotoBookView::slotImageSizeSliderPressed() {
+
+    m_sliderPressed = true;
+}
+
+
+void KPhotoBookView::slotImageSizeSliderReleased() {
+
+    m_sliderPressed = false;
+
+    updateCurrentImageSizeLabel();
+
+    // slider released after dragging it --> redraw previews
+    updateCurrentImageSize();
+}
+
+
+void KPhotoBookView::slotImageSizeSliderChanged(int size) {
+    size = size;
+
+    updateCurrentImageSizeLabel();
+
+    // slider position changed without dragging it --> redraw previews
+    if (!m_sliderPressed) {
+        updateCurrentImageSize();
+    }
+}
+
+
+void KPhotoBookView::slotShowCurrentImage() {
+
+    KFileItem* item = m_fileView->currentFileItem();
+    KURL file = item->url();
+
+    kdDebug() << "Showing file in kuickshow. url=<" << file.url() << ">, path=<" << file.path() << ">" << endl;
+
+    KProcess *proc = new KProcess();
+
+    *proc << "kuickshow";
+    *proc << file.path();
+    proc->start(KProcess::DontCare);
+}
+
+
+//
+// private methods
+//
+void KPhotoBookView::updateCurrentImageSizeLabel() {
+
+    QString text = QString("%1 x %2").arg(8*m_imageSizeSlider->value()).arg(8*m_imageSizeSlider->value());
+    m_currentImageSize->setText(text);
+}
+
+
+void KPhotoBookView::updateCurrentImageSize() {
+
+    // remove all displayed images
+    removeAllFiles();
+
+    m_fileView->setPreviewSize(8*m_imageSizeSlider->value());
+    m_fileView->showPreviews();
+    m_fileView->setGridX(8*m_imageSizeSlider->value()+10);
+    m_fileView->setGridY(8*m_imageSizeSlider->value()+10);
+
+    // force redrwaing all files by removing them and adding them again
+    updateFiles();
+}
+
+
+void KPhotoBookView::removeAllFiles() {
+
+    QPtrList<QIconViewItem>* itemList = new QPtrList<QIconViewItem>();
+
+    for (QIconViewItem* item = m_fileView->firstItem(); item; item = item->nextItem()) {
+        itemList->append(item);
+    }
+
+    for (QIconViewItem* item = itemList->first(); item; item = itemList->next()) {
+        m_fileView->takeItem(item);
+    }
+
+    delete itemList;
+}
+
+
+#include "kphotobookview.moc"
