6e90c5acf77db45301ced2ab03ddf3926bc36c7b
starcube <starcube@99f15ee9-15f3-0310-9699-9f69ca06e6ba>
2004-03-07 18:51:55 +0000

    Initial revision

---
+++ trunk/kphotobook/kphotobooksrc/src/tagtree.h
@@ -0,0 +1,72 @@
+/***************************************************************************
+ *   Copyright (C) 2003 by Michael Christen                                *
+ *   starcube@my-mail.ch                                                   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#ifndef _TAGTREE_H_
+#define _TAGTREE_H_
+
+#include <klistview.h>
+#include <kpopupmenu.h>
+
+#include <qptrlist.h>
+#include <qstring.h>
+#include <qlistview.h>
+#include <qpoint.h>
+
+class TagNode;
+class TagTreeNode;
+class KPhotoBook;
+
+/**
+ * The tagtree (can display checkboxes in the columns).
+ *
+ * CVS-ID $Id: tagtree.h,v 1.1 2004/03/07 18:52:42 starcube Exp $
+ */
+class TagTree : public KListView {
+
+    Q_OBJECT
+
+public:
+    static const int COLUMN_TEXT = 0;
+    static const int COLUMN_VALUE = 1;
+    static const int COLUMN_FILTER = 2;
+
+    TagTree(QWidget* parent, KPhotoBook* photobook, const char* name);
+    ~TagTree() {
+    }
+
+    void addTagNodes(QPtrList<TagNode>* rootNodeList);
+    void addTagNode(TagNode* rootNode);
+    void addTagNode(TagTreeNode* parent, TagNode* child);
+
+    KPhotoBook* photobook() {
+        return m_photobook;
+    }
+
+private slots:
+    void slotListViewClicked(int button, QListViewItem* item, const QPoint& point, int column);
+    void slotListViewDoubleClicked(QListViewItem* item, const QPoint& point, int column);
+
+private:
+    void buildTagNodeTree(TagTreeNode* parent, QPtrList<TagNode>* children);
+
+    KPhotoBook* m_photobook;
+};
+
+#endif
---
+++ trunk/kphotobook/kphotobooksrc/src/tagtreenodesourcedir.h
@@ -0,0 +1,73 @@
+/***************************************************************************
+ *   Copyright (C) 2003 by Michael Christen                                *
+ *   starcube@my-mail.ch                                                   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#ifndef TAGTREENODESOURCEDIR_H
+#define TAGTREENODESOURCEDIR_H
+
+#include "tagtreenode.h"
+
+class SourceDir;
+
+class TagTreeNodeSourceDir : public TagTreeNode {
+
+public:
+    TagTreeNodeSourceDir(KListView* parent, SourceDir* sourceDir, KPhotoBook* photobook, KPopupMenu* contextMenu = 0);
+    TagTreeNodeSourceDir(KListViewItem* parent, SourceDir* sourceDir, KPhotoBook* photobook, KPopupMenu* contextMenu = 0);
+
+    virtual ~TagTreeNodeSourceDir() {
+    }
+
+    TagNode* tagNode() {
+        return 0;
+    }
+
+    SourceDir* sourceDir() {
+        return m_sourceDir;
+    }
+
+    void setSelectedFilesCount(int selectedFilesCount);
+    void increaseSelectedFilesCount(int amount) {
+        setSelectedFilesCount(m_selectedFilesCount + amount);
+    }
+    int selectedFilesCount() {
+        return m_selectedFilesCount;
+    }
+
+    void setIncluded(bool included);
+    bool included();
+    void invertInclusion();
+
+    void setIncludedRecursive(bool included);
+    void invertInclusionRecursive();
+
+    void setOpenRecursive(bool open);
+
+    virtual void columnClicked(TagTree* tagTree, int column);
+    virtual void paintCell(QPainter* p, const QColorGroup& cg, int column, int width, int alignment);
+
+private:
+    SourceDir* m_sourceDir;
+
+    int m_selectedFilesCount;
+
+    void init(bool showRelativePath);
+};
+
+#endif
