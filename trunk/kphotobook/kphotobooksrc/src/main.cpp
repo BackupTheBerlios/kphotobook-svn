3207983f979ff2548632737012367ae064df1220
starcube <starcube@99f15ee9-15f3-0310-9699-9f69ca06e6ba>
2004-06-20 08:41:08 +0000

    - reformatted code - texts should be translatable now - text added

--- trunk/kphotobook/kphotobooksrc/src/main.cpp
+++ trunk/kphotobook/kphotobooksrc/src/main.cpp
@@ -14,63 +14,63 @@
 #include <kdebug.h>
 
 static const char description[] = I18N_NOOP(
-                                      "KPhotoBook is a sophisticated and highly optimized KDE\n"
-                                      "application for managing and refinding your digital photos."
-                                  );
+  "KPhotoBook is a sophisticated and highly optimized KDE\n"
+  "application for managing and refinding your digital photos."
+);
 
 static const char version[] = "0.0.6";
 
 static KCmdLineOptions options[] = {
-                                       { "+[URL]", I18N_NOOP( "Document to open." ), 0 },
-                                       KCmdLineLastOption
-                                   };
+  { "+[URL]", I18N_NOOP( "Document to open." ), 0 },
+  KCmdLineLastOption
+};
 
 int main(int argc, char** argv) {
 
-    KAboutData about("kphotobook", I18N_NOOP("KPhotoBook"), version, description,
-                     KAboutData::License_GPL, "(C) 2003 Michael Christen",
-                     0, // free text, can contain new lines
-                     "http://kphotobook.sourceforge.net",
-                     "starcube@users.sourceforge.net");
-    about.addAuthor("Michael Christen", "The master chief developer.", "starcube@users.sourceforge.net" );
-    about.addAuthor("Thomas Christen", "One of Santa's greater helpers.\nHelps me creating icons and writing the website and the documentation.", "chtom@users.sourceforge.net");
-    about.addAuthor("Stefan Fink", "One of Santa's little helpers.\nAdvises in design and usability questions.");
-    about.addAuthor("Daniel Gerber", "One of Santa's little helpers.\nAdvises in design and usability questions.");
+  KAboutData about("kphotobook", I18N_NOOP("KPhotoBook"), version, description,
+                    KAboutData::License_GPL, "(C) 2003 Michael Christen",
+                    0, // free text, can contain new lines
+                    "http://kphotobook.sourceforge.net",
+                    "starcube@users.sourceforge.net");
+  about.addAuthor("Michael Christen", I18N_NOOP("The master chief developer."), "starcube@users.sourceforge.net" );
+  about.addAuthor("Thomas Christen", I18N_NOOP("One of Santa's greater helpers.\nHelps me creating icons and writing the website and the documentation."), "chtom@users.sourceforge.net");
+  about.addAuthor("Stefan Fink", I18N_NOOP("One of Santa's little helpers.\nAdvises in design and usability questions.\n(He is also responsible for the KPhotoBook icon...)"));
+  about.addAuthor("Daniel Gerber", I18N_NOOP("One of Santa's little helpers.\nAdvises in design and usability questions."));
 
-    about.addCredit("George W. Bush, President of the USA", "For being a stupid little git.", "idiot@whitehouse.org" );
+  about.addCredit("George W. Bush, President of the USA", I18N_NOOP("For being a stupid little git."), "idiot@whitehouse.org" );
 
-    KCmdLineArgs::init(argc, argv, &about);
-    KCmdLineArgs::addCmdLineOptions(options);
+  KCmdLineArgs::init(argc, argv, &about);
+  KCmdLineArgs::addCmdLineOptions(options);
 
-    KApplication app;
+  KApplication app;
 
-    KMdi::MdiMode mdiMode = KMdi::IDEAlMode;
-    if (Settings::generalViewMode() == Settings::EnumGeneralViewMode::TabPageMode) {
-        mdiMode = KMdi::TabPageMode;
+  KMdi::MdiMode mdiMode = KMdi::IDEAlMode;
+  if (Settings::generalViewMode() == Settings::EnumGeneralViewMode::TabPageMode) {
+      mdiMode = KMdi::TabPageMode;
+  }
+
+  KCmdLineArgs* args = KCmdLineArgs::parsedArgs();
+  if (args->count() == 0) {
+
+    KPhotoBook* widget = new KPhotoBook(mdiMode);
+    widget->show();
+
+    // try to load last opened file
+    QString lastFileName = Settings::fileSystemLastOpenedFile();
+    if (!lastFileName.isEmpty()) {
+      QFileInfo lastFile(lastFileName);
+      widget->load(lastFile);
     }
-
-    KCmdLineArgs* args = KCmdLineArgs::parsedArgs();
-    if (args->count() == 0) {
-
-        KPhotoBook* widget = new KPhotoBook(mdiMode);
-        widget->show();
-
-        // try to load last opened file
-        QString lastFileName = Settings::fileSystemLastOpenedFile();
-        if (!lastFileName.isEmpty()) {
-            QFileInfo lastFile(lastFileName);
-            widget->load(lastFile);
-        }
-    } else {
-        int i = 0;
-        for (; i < args->count(); i++) {
-            QFileInfo file(args->url(i).path());
-            KPhotoBook* widget = new KPhotoBook(mdiMode);
-            widget->show();
-            widget->load(file);
-        }
+  } else {
+    int i = 0;
+    for (; i < args->count(); i++) {
+      QFileInfo file(args->url(i).path());
+      KPhotoBook* widget = new KPhotoBook(mdiMode);
+      widget->show();
+      widget->load(file);
     }
-    args->clear();
+  }
+  args->clear();
 
-    return app.exec();
+  return app.exec();
 }
